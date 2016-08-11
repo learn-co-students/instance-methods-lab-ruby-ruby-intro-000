0f7e568fdeddba82b6c4df4a2518752534f9d88a
ayo wynter <ayo.wynter@yahoo.com>
2016-08-11 00:59:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+Class Dog
+def bark
+puts"Woof!"
+end
+def sit
+puts"The Dog is sitting"
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+Class Person
+def talk
+puts"Hello World!"
+end
+def walk
+puts"The Person is walking"
+end
+end
