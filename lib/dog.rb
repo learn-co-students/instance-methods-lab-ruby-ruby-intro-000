556a6d3661dd08c3e0b78991feb9c3cdc82f78e5
Marek Koseski <soroz30@yahoo.pl>
2016-09-05 06:42:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+  def talk
+    puts "Hello world!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
