e582aeb1982f0aecaad28a9dd9df28ec47a9be8d
Mary Benitez <meebenitez@gmail.com>
2017-05-02 02:21:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  #dog class code
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+  
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
