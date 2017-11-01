83336b40e9291a265b79134e91865aa7922ce4e2
Brandon Collins <brandon@flatironschool.com>
2017-11-01 16:31:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+end
+
+
+# def bark
+#   puts "Woof!"
+# end
+# end
+
+# def sit
+#   puts "The Dog is sitting"
+# end
+# end
+
+# fido = Dog.new
+# fido.bark
+
+# snoopy = Dog.new
+# snoopy.sit
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+end
+
+# def talk
+#   puts "Hello World!"
+# end
+# end
+
+# def walk
+#   puts "The Person is walking"
+# end
+# end
+
+# Brandon = person.new
+# Brandon.talk
+
+# Brandon = person.new
+# Brandon.walk
