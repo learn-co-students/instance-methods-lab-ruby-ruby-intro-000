cf741e9f9608cacbfb213653a53eaf959aa69b89
Marcus Jun <mjun@calstate.edu>
2018-03-25 17:29:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
