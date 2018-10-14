49d71f429a64c754e77894a25cda9760627e065a
Khamis Manzu <kmanzu@yahoo.com>
2018-10-14 11:05:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,26 @@
 class Dog
+  
   def bark
     
     puts "Woof!"
   
+  end
+  
+  def sit
+    
+    puts "The Dog is sitting"
+    
   end
 end
 
 fido = Dog.new
 
 fido.bark
+
+snoopy = Dog.new
+
+snoopy.sit
+
+
+
+
