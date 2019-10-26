ff15bf0e80a8b66ac25b811f6d5c3b2e2616a0f3
Gary Schwaeber <gschwaeb@gmail.com>
2019-10-26 16:00:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+end
+
+class Dog
+  def bark
+    puts "Woof!"
+  end
+end
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+end 
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+end
+
+class Person
+  def walk
+    puts "The Person is walking"
+  end
+end
