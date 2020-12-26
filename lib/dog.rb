8de77bc72c214ad7aeafe345292fd87f694e4e61
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-26 23:47:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
   def bark
     puts "Woof!"
+  end
   def sit
     puts "The Dog is sitting"
   end
--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,8 @@
 class Person
   def talk
-    puts "Hello World"
-  def wealk
+    puts "Hello World!"
+  end
+  def walk
     puts "The Person is walking"
   end
 end
